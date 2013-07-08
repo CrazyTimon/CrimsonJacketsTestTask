9fdd5645e5140a59a98caadaa29a18b1bc82e80a
CrazyTimon <crazytimon88@gmail.com>
2013-07-09 00:12:53 +0600

    v_0_1

--- js/main.js
+++ js/main.js
@@ -1,50 +1,144 @@
-"use strict";
+
+var dataJSON = {
+    menu:[
+        {
+            title:"о компании",
+            pos:{
+                x:100,
+                y:50
+            }
+        },
+        {
+            title:"аппаратное обеспечение",
+            pos:{
+                x:250,
+                y:150
+            },
+            menu:[{
+                title: "аппаратная линейка",
+                pos:{
+                    x:0,
+                    y:0
+                }
+            },{
+                title: "серверные решения",
+                pos:{
+                    x:20,
+                    y:20
+                }
+            },{
+                title: "система хранения данных",
+                pos:{
+                    x:30,
+                    y:30
+                }
+            },{
+                title: "компьютерные кластеры",
+                pos:{
+                    x:80,
+                    y:110
+                }
+            }]
+        },
+        {
+            title:"програмные решения",
+            pos:{
+                x:380,
+                y:250
+            }
+        },
+        {
+            title:"партнеры",
+            pos:{
+                x:480,
+                y:380
+            }
+        },
+        {
+            title:"контакты",
+            pos:{
+                x:480,
+                y:680
+            }
+        }
+        ]
+    };
+         //"апаратное обеспечение","програмные решения","партнеры","контакты"]
+
 
 var menu_elements = Backbone.View.extend({
+    events:{
+        "click": "onElementClick"
+    },
+    activated: false,
     initialize: function(){
         this.position = this.$el.offset(); 
         this.$el.offset({top:0,left:0});
+        this.main_list = this.options.main_list;
+        this.index = this.options.index;
+        this.top = this.$el.offset().top;
+        this.left = this.$el.offset().left;
     },
-    goTop: function(){
-        this.$el.offset({top:0});
+    onElementClick:function(){
+        var that = this;
+        this.activated = true;
+        this.$el.animate({
+            top: 50,
+            left: 200
+        }, {
+          step: function(pos, handler) {
+            if(handler.prop === "top"){
+                that.top = pos;
+                console.log(that.top);
+            } else if(handler.prop === "left"){                
+                that.left = pos;
+            }
+            that.main_list.render();
+          }
+        }, 2000);
+    },
+    updateCoord: function(){
+        this.top = this.$el.offset().top;
+        this.left = this.$el.offset().left;
+    },
+    getPrev: function(){
+        return this.main_list.menu_elements[this.index-1] ? this.main_list.menu_elements[this.index-1] : null;
+    },
+    getNext: function(){
+        return this.main_list.menu_elements[this.index+1] ? this.main_list.menu_elements[this.index+1] : null;
     }
 });
 
+var testModel = Backbone.Model.extend({
+});
+
 var menu = Backbone.View.extend({
-    
-    events: {
-        "click .menu__element": "onElementClick"
-    },
+    model: new testModel(),
     initialize: function(){
         var that = this;
+        //TODO брать данные с сервера
+        this.model.set(dataJSON);
+        //TODO брать данные с сервера
         this.menu_elements = [];
-        $.each(this.$(".menu__element"), function(){
-            that.menu_elements.push( new menu_elements({el:this}) );
+        $.each(this.$(".menu__element"), function(i,v){
+            that.menu_elements.push( new menu_elements({el:this, main_list:that, index:i}) );
         });
+        debugger;
         that.render();
-        $.each(that.menu_elements, function(){
+        $.each(that.menu_elements, function(i,v){
+            var that_element = this;
             this.$el.animate({
                 top: this.position.top,
                 left: this.position.left
             }, {
               step: function(x,y) {
                 that.render();
+              },
+              complete: function(){
+                that_element.updateCoord();
               }
             }, 2000);
         });
-    },
-    jumping: function(){
-
-    },
-    onElementClick:function(e){
-        var that = this;
-        $(e.currentTarget).animate({
-            top: 50
-        }, {
-          step: function() {
-            that.render();
-          }
-        }, 2000);
     },
     render: function() {
         var that = this;
@@ -52,7 +146,6 @@ var menu = Backbone.View.extend({
         $("#canvas").attr("height", document.height + "px");
         var canvas=document.getElementById("canvas");
         var x=canvas.getContext("2d");
-        debugger;
         x.beginPath();
         x.strokeStyle = "#FF0000";
         x.stroke();
@@ -72,4 +165,4 @@ var menu = Backbone.View.extend({
 
 });
 
-var  menuObj = new menu({el:$(".menu")});
+menuObj = new menu({el:$(".menu")});
